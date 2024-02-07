9c9d9ab8dff89cc93f625718cbb48ac3a733e33b
Balazs Farkas <94067165+BFarkascode@users.noreply.github.com>
2024-02-07 10:17:22 +0100

    Update ClockDriver_STM32L0x3.h

--- ClockDriver_STM32L0x3.h
+++ ClockDriver_STM32L0x3.h
@@ -1,8 +1,11 @@
 /*
- * ClockDriver_STM32L0x3.h			v.1.0
- *
  *  Created on: Oct 30, 2023
- *      Author: Balazs Farkas
+ *  Project: STM32_ClockDriver
+ *  File: ClockDriver_STM32L0x3.h
+ *  Author: BalazsFarkas
+ *  Processor: STM32L053R8
+ *  Compiler: ARM-GCC (STM32 IDE)
+ *  Program version: 1.0
  */
 
 #ifndef INC_RCCTIMPWMDELAY_CUSTOM_H_
